584d34d6c4b4b2a069da500fb9680ff31cd51db5
Patches Jynessa <patchesj@student.21-school.ru>
2025-03-24 13:14:19 +0300

    fix: FED-2 underscore deleted in sh

--- migrations/entrypoint_up_one.sh
+++ migrations/entrypoint_up_one.sh
@@ -3,4 +3,4 @@
 # Применим миграции
 # ВАЖНО! Для локального запуска вызвать из корня проекта, предварительно подгрузив переменные окружения в терминал командой  `set -a; source <путь к .env файлу>; set +a`
 # Установка goose: go install github.com/pressly/goose/v3/cmd/goose@latest
-goose -dir ./migrations postgres "user=$FEED_SERVICE__POSTGRES_USER password=$FEED_SERVICE__POSTGRES_PASSWORD dbname=$FEED_SERVICE__POSTGRES_DB host=$FEED_SERVICE__POSTGRES_HOST port=$FEED_SERVICE__POSTGRES_PORT sslmode=disable" up-by-one
+goose -dir ./migrations postgres "user=$FEED_SERVICE_POSTGRES_USER password=$FEED_SERVICE_POSTGRES_PASSWORD dbname=$FEED_SERVICE_POSTGRES_DB host=$FEED_SERVICE_POSTGRES_HOST port=$FEED_SERVICE_POSTGRES_PORT sslmode=disable" up-by-one
